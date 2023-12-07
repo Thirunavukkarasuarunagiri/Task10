f56929fb4f2016b4424f7f9c11aeea41d553f9cd
Thirunavukkarasuarunagiri <151116146+Thirunavukkarasuarunagiri@users.noreply.github.com>
2023-12-07 13:40:27 +0530

    Add files via upload

---
+++ Task_10_1_req.txt.py
@@ -0,0 +1,9 @@
+# using the python PIP command create a "requirement.txt" file from terminal/command-prompt of your system
+
+#1.Open your terminal or command prompt.
+
+#2.Navigate to the directory which to be used.
+
+#3.Run the following command to generate the requirements.txt file,
+
+pip freeze > requirements.txt
---
+++ Task_10_2_pip_flask.py
@@ -0,0 +1,5 @@
+#using the python PIP command install the flask module < 2.0 into your system
+
+pip install Flask==<version>
+
+pip install Flask==1.2.23
